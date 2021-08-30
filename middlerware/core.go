6a746af092b75c21b50ef7eda008d19f762aa7be
peterliang233 <ncuyanping666@126.com>
2021-08-30 19:21:03 +0800

    :sparkles: 实现跨域中间件

--- middlerware/core.go
+++ middlerware/core.go
@@ -1,1 +1,20 @@
 package middlerware
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"time"
+)
+
+// Cors 跨域中间件
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		cors.New(cors.Config{
+			AllowOrigins:  []string{"*"},
+			AllowMethods:  []string{"*"},
+			AllowHeaders:  []string{"*"},
+			ExposeHeaders: []string{"content-Length", "Authorization"},
+			MaxAge:        12 * time.Hour,
+		})
+	}
+}
--- router/router.go
+++ router/router.go
@@ -2,20 +2,20 @@ package router
 
 import (
 	"github.com/Peterliang233/go-chat/config"
+	"github.com/Peterliang233/go-chat/middlerware"
 	"github.com/gin-gonic/gin"
 )
 
 // InitRouter 初始化相关路由
 func InitRouter() *gin.Engine {
-
 	gin.SetMode(config.AppMode)
 
 	router := gin.New()
 
-	router.Use()
-	{
-		router.POST("/chat")
-	}
+	router.Use(middlerware.Cors())
+	router.Use(middlerware.Cors())
+
+	router.POST("/chat")
 
 	return router
 }
