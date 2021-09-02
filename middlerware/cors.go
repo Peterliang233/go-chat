c3d202e1bb04aea26a232dd89ef8d3b061fbbc60
peterliang233 <ncuyanping666@126.com>
2021-09-02 18:01:07 +0800

    :zap: 修改跨域中间件

--- middlerware/cors.go
+++ middlerware/cors.go
@@ -15,11 +15,9 @@ func Cors() gin.HandlerFunc {
 		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
 		c.Header("Access-Control-Allow-Credentials", "true")
 
-		// 放行所有OPTIONS方法
 		if method == "OPTIONS" {
 			c.AbortWithStatus(http.StatusNoContent)
 		}
-		// 处理请求
 		c.Next()
 	}
 }
